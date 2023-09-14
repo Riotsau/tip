eaa36c842465c94499ebc7a73adb999720e2629a
Aaron <aaroncareer.au@gmail.com>
2023-09-14 19:56:27 +1000

    student management

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
+const axiosStudentInstance = axios.create({
+  baseURL: 'https://crudcrud.com/api/37f1084d5245403b992ccf816291e738',
+});
+// Add a request interceptor
+axiosStudentInstance.interceptors.request.use(function (config) {
+  console.log('requst', config);
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axiosStudentInstance.interceptors.response.use(function (response) {
+  console.log('response', response);
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+const createStudent = (name, age) => {
+  axiosStudentInstance.post('/student', {
+    name: name,
+    age: age,
+  });
+}
+
 export default function App() {
   return (
     <div>
@@ -39,14 +68,35 @@ const StudentEditor = () => {
   const handleOnClick = () => {
     console.log('name', name);
     console.log('age', age);
+    createStudent(name, age);
+  }
+  const [students, setStudents] = useState([]);
+  const handleOnGetStudent = () => {
+    axiosStudentInstance.get('/student').then((response) => setStudents(response.data));
+  }
+  const handleDelete = (id) => {
+    axiosStudentInstance.delete(`/student/${id}`);
   }
   return (
     <div>
-      <input type="text" onChange={(e) => setName(e.target.value)}></input>
-      <input type="text" onChange={(e) => setAge(e.target.value)}></input>
-      <button onClick={handleOnClick}>
-        Submit
-      </button>
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)}></input>
+        <input type="text" onChange={(e) => setAge(e.target.value)}></input>
+        <button onClick={handleOnClick}>
+          Submit
+        </button>
+        <button onClick={handleOnGetStudent}>
+          Get student
+        </button>
+      </div>
+      <div>
+          {students.map(student => (
+            <div key={student._id}>
+              <div>name: {student.name} age: {student.age}</div>
+              <button onClick={() => handleDelete(student._id)}>Delete</button>
+            </div> 
+          ))}
+        </div>
     </div>
   )
 }
